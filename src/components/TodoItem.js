1d836b5b9e4e563e8bde9bc0b3f4d6cfcce479d1
evaldasv <evaldasv@gmail.com>
2016-03-09 20:52:33 +0200

    add X close button and modify styling.

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -16,14 +16,35 @@ var TodoItem = React.createClass({
         TodoActions.toggleTodo(props.id, {completed: ! props.completed});
     },
 
+    handleMouseOver: function(e) {
+        e.preventDefault();
+        this.toggleClass(' hover');
+    },
+
+    handleMouseOut: function(e) {
+        e.preventDefault();
+        this.toggleClass();
+    },
+
+    toggleClass: function(klass) {
+        this.refs.closeButton.className = klass || '';
+    },
+
+    handleRemove: function() {
+        TodoActions.removeTodo(this.props.id);
+    },
+
     render: function() {
         var style = {textDecoration: this.props.completed ? 'line-through' : 'none' };
         return (
-            <li>
-                <a style={style} href="#"
+            <li style={style} className="todo-item">
+                <span
+                    onMouseOver={this.handleMouseOver}
+                    onMouseOut={this.handleMouseOut}
                     onClick={this.handleClick}>
                     {this.props.text}
-                </a>
+                    <button ref="closeButton" className="close-button" onClick={this.handleRemove}>x</button>
+                </span>
             </li>
         );
     }
